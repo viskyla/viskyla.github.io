749f526ddf85a811c35a0139462032198527c819
Viskyla <91972350+viskyla@users.noreply.github.com>
2025-04-01 10:48:45 -0400

    topbar

--- navbar.js
+++ navbar.js
@@ -1,8 +1,39 @@
 const header = `
     <style>
+    	*{
+        	margin: 0;
+            padding: 0;
+        }
+        .navbar{
+            background-color: #333;
+            overflow: hidden;
+        }
+
+        .navbar a{
+            float: left;
+            color: #f2f2f2;
+            padding: 5px;
+            text-align: center;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .navbar ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+            position: -webkit-sticky; /* Safari */
+            position: sticky;
+            top: 0;
+        }
     </style>
     <div class="navbar">
-        <a href="index.html">Goon</a>
+        <ul>
+            <li><a href="https://viskyla.com">Home</a></li>
+            <li><a href="https://viskyla.com">Also home but another button</a></li>
+        </ul>
     </div>
 `;
 document.querySelector("body").insertAdjacentHTML("afterbegin", header);
