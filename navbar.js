4fca50fc7c946d6fc8252c6c4e6a64c8a403ab64
viskyla <novapeache@gmail.com>
2025-04-01 12:03:59 -0500

    coolio

--- navbar.js
+++ navbar.js
@@ -1,11 +1,20 @@
 const header = `
     <style>
+
+        @font-face {
+            font-family: andy;
+            src: url(ANDYB.TTF);
+        }
+
     	*{
         	margin: 0;
             padding: 0;
+            font-family: andy;
         }
 
-
+        body{
+            padding: 10px;
+        }
             
         .navbar{
             background-color: #333;
@@ -15,28 +24,37 @@ const header = `
         .navbar a{
             float: left;
             color: #f2f2f2;
-            padding: 5px;
+            padding:10px;
             text-align: center;
             text-decoration: none;
             font-size: 17px;
         }
 
+        .navbar a:hover{
+            background-color: #444;
+            color: #ffffa6;
+        }
+
         .navbar ul {
             list-style-type: none;
-            margin: 0;
-            padding: 0;
             overflow: hidden;
             background-color: #333;
-            position: -webkit-sticky; /* Safari */
+            position: -webkit-sticky;
             position: sticky;
             top: 0;
+        }
+
+        .navbar li:last-child{
+            float:right;
         }
     </style>
     <div class="navbar">
         <ul>
-            <li><a href="https://viskyla.com">Home</a></li>
-            <li><a href="https://viskyla.com">Also home but another button</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Projets</a></li>
+            <li><a href="">About</a></li>
         </ul>
     </div>
 `;
-document.querySelector("head").insertAdjacentHTML("afterbegin", header);
+document.querySelector("head").insertAdjacentHTML("afterend", header);
